8d0e1da06942a3411d25e9d7aa530280b328e60a
ElijahPepe <business@elijahpepe.com>
2021-08-21 13:56:55 -0700

    Remove header as per Exzap's recommendation

--- CemublueTemplate.php
+++ CemublueTemplate.php
@@ -77,31 +77,6 @@ class CemublueTemplate extends BaseTemplate {
 		<a href="#top">content</a>
 	</div>
 	<header>
-		<nav class="navbar navbar-default navbar-fixed-top">
-    		<div class="container">
-    	    	<div class="navbar-header">
-        			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-         				<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-        			    <span class="icon-bar"></span>
-           				<span class="icon-bar"></span>
-        			</button>
-          		<a class="navbar-brand" href="index.html">Cemu</a>
-        		</div>
-	        	<div id="navbar" class="navbar-collapse collapse">
-    	   			<ul class="nav navbar-nav">
-            		<li><a href="index.html">Home</a></li>  
-         			<li><a href="#download">Download</a></li>
-            		<li><a href="community.html">Community</a></li>
-            		<li><a href="changelog.html">Changelog</a></li>
-            		<li><a href="faq.html">FAQ</a></li>
-            		<li><a href="http://compat.cemu.info/">Compatibility</a></li>
-            		<li class="active"><a href="http://compat.cemu.info/wiki/Main_Page">Wiki</a></li>
-		    		<li><a href="donate.html">Donate</a></li>
-        		</ul>
-        		</div>
-   	   		</div>
-    	</nav>
 		<nav class="cemublue-navbar" role="navigation">
 			<div class="container">
 				<div class="row">
